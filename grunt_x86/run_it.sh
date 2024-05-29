c56189153f2510ad4ff813c4a4f2441ea3aa9fec
pondersome <karim@compuguru.com>
2024-05-29 15:00:56 -0500

    new docker config in process

---
+++ grunt_x86/run_it.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script uses Docker to launch a container in which to run ROS
+
+set -u # Treat empty variables as an error.
+
+RM="--rm"
+
+# support the -keep argument to not remove the container upon exit
+if [ $# -ne 0 ]; then
+  if [ $1 == "-keep" ]; then
+    RM=""; shift 1; echo "not removing container"
+  else
+    echo "Usage: $0 [-keep]"; exit
+  fi
+fi    
+
+echo "running ${ACTIVE_DOCKER} docker with home " $HOME " user " $USER 
+docker run                                                     \
+    --net=host                                                 \
+    -t                                                         \
+    ${RM}                                                      \
+    --privileged                                               \
+    -v $HOME/.bash_history:$HOME/.bash_history                 \
+    -v $HOME/ros2_ws:$HOME/ros2_ws                             \
+    -v $HOME/bag_files:$HOME/bag_files                         \
+    -v /tmp/.X11-unix:/tmp/.X11-unix                           \
+    -v /tmp:/tmp                                               \
+    --user $USER                                               \
+    -w $HOME                                                   \
+    -i                                                         \
+    ${ACTIVE_DOCKER}                                           \
+    $@
+
